da80715af9c20d40531adb17a546a61d5f6a25e6
Victoria Kastanenka <vkastanenka@gmail.com>
2020-06-16 15:18:39 -0400

    refactored about section

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -8,25 +8,25 @@ const NavBar = () => {
   return (
     <nav className="nav">
       <ul className="nav__list">
-        <li className="nav__list-item fw-medium">
+        <li className="nav__list-item">
           <ScrollLink section="home">Home</ScrollLink>
         </li>
-        <li className="nav__list-item fw-medium">
+        <li className="nav__list-item">
           <ScrollLink section="about">About</ScrollLink>
         </li>
-        <li className="nav__list-item fw-medium">
+        <li className="nav__list-item">
           <ScrollLink section="portfolio">Portfolio</ScrollLink>
         </li>
-        <li className="nav__list-item fw-medium">
+        <li className="nav__list-item">
           <ScrollLink section="skills">Skills</ScrollLink>
         </li>
-        <li className="nav__list-item fw-medium">
+        <li className="nav__list-item">
           <ScrollLink section="testimonials">Testimonials</ScrollLink>
         </li>
-        <li className="nav__list-item fw-medium">
+        <li className="nav__list-item">
           <ScrollLink section="contact">Contact</ScrollLink>
         </li>
-        <li className="nav__list-item fw-medium">
+        <li className="nav__list-item">
           <ScrollLink section="resume">Resume</ScrollLink>
         </li>
       </ul>
--- src/pages/Landing/Layout/About.js
+++ src/pages/Landing/Layout/About.js
@@ -6,69 +6,73 @@ import Icon from "../../../components/Icon/Icon";
 
 const About = () => {
   return (
-    <section className="section about fc-grey-dark-3" id='about'>
-      <h2 className="heading-secondary fc-primary-dark ma-bt--md text-shadow">About Me</h2>
-      <div className="about__introduction">
-        <h3 className="heading-tertiary">
-          Allow me to formally introduce myself. I'm Victoria.
-        </h3>
-        <p className="text-large">
-          Since beginning my coding journey one year ago, I have dedicated
-          myself to the process of web development, becoming acquainted with
-          both the front and back ends of the process. My diligence has allowed
-          me to gain real world experience working with clients to bring their
-          ideas to life. No matter the task, I always persevere to bring 100% to
-          both myself, and those that I work with.{" "}
-        </p>
-      </div>
-      <div className="about__perks">
-        <div className="about__perk">
-          <div className="about__perk-icon">
-            <Icon
-              type="user"
-              className="icon icon--massive icon--primary-dark"
-            />
-          </div>
-          <p className="text-larger fw-medium">Client Experience</p>
-          <p className="text-large pd-x--lg">
-            Strong interaction for maximum satisfaction
+    <section className="about fc-grey-dark-3" id="about">
+      <div className="section about__content ta-center">
+        <h2 className="heading-secondary section__heading">
+          About Me
+        </h2>
+        <div className="about__introduction ma-bt--7">
+          <h3 className="heading-tertiary ma-bt--1">
+            Allow me to formally introduce myself. I'm Victoria.
+          </h3>
+          <p className="text-large">
+            Since beginning my coding journey one year ago, I have dedicated
+            myself to the process of web development, becoming acquainted with
+            both the front and back ends of the process. My diligence has
+            allowed me to gain real world experience working with clients to
+            bring their ideas to life. No matter the task, I always persevere to
+            bring 100% to both myself, and those that I work with.{" "}
           </p>
         </div>
-        <div className="about__perk">
-          <div className="about__perk-icon">
-            <Icon
-              type="laptop"
-              className="icon icon--massive icon--primary-dark"
-            />
+        <div className="about__perks">
+          <div className="about__perk">
+            <div className="about__perk-icon">
+              <Icon
+                type="user"
+                className="icon icon--massive icon--primary-dark"
+              />
+            </div>
+            <p className="text-larger fw-medium">Client Experience</p>
+            <p className="text-large pd-x--lg">
+              Strong interaction for maximum satisfaction
+            </p>
           </div>
-          <p className="text-larger fw-medium">Launched Websites</p>
-          <p className="text-large pd-x--lg">
-            Individually made from start to finish
-          </p>
-        </div>
-        <div className="about__perk">
-          <div className="about__perk-icon">
-            <Icon
-              type="graduation-cap"
-              className="icon icon--massive icon--primary-dark"
-            />
+          <div className="about__perk">
+            <div className="about__perk-icon">
+              <Icon
+                type="laptop"
+                className="icon icon--massive icon--primary-dark"
+              />
+            </div>
+            <p className="text-larger fw-medium">Launched Websites</p>
+            <p className="text-large pd-x--lg">
+              Individually made from start to finish
+            </p>
           </div>
-          <p className="text-larger fw-medium">Quick Learner</p>
-          <p className="text-large pd-x--lg">
-            Self taught in full stack development
-          </p>
-        </div>
-        <div className="about__perk">
-          <div className="about__perk-icon">
-            <Icon
-              type="network"
-              className="icon icon--massive icon--primary-dark"
-            />
+          <div className="about__perk">
+            <div className="about__perk-icon">
+              <Icon
+                type="graduation-cap"
+                className="icon icon--massive icon--primary-dark"
+              />
+            </div>
+            <p className="text-larger fw-medium">Quick Learner</p>
+            <p className="text-large pd-x--lg">
+              Self taught in full stack development
+            </p>
           </div>
-          <p className="text-larger fw-medium">Adept</p>
-          <p className="text-large pd-x--lg">
-            Adapt to new tech for each project's needs
-          </p>
+          <div className="about__perk">
+            <div className="about__perk-icon">
+              <Icon
+                type="network"
+                className="icon icon--massive icon--primary-dark"
+              />
+            </div>
+            <p className="text-larger fw-medium">Adept</p>
+            <p className="text-large pd-x--lg">
+              Adaptable to new tech for each project's needs
+            </p>
+          </div>
         </div>
       </div>
     </section>
